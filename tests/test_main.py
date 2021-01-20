7e8c2c1a8934212dc04c18e26e1aceffa94b58ae
RoJac88 <rodrigo.jacob@gmail.com>
2021-01-19 22:09:25 -0300

    tests

---
+++ tests/test_main.py
@@ -0,0 +1,13 @@
+from fastapi.testclient import TestClient
+from api.main import app
+
+client = TestClient(app)
+
+
+def test_index():
+    response = client.get('/')
+    assert response.status_code == 200
+    assert response.json() == {
+        'secret': 'shhhhh',
+        'ps': 'don\'t tell anyone'
+    }
